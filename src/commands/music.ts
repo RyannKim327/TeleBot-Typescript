c60d09234948694446a31e1bd7c23b829fe27383
RyannKim327 <rksesgundo123@gmail.com>
2024-03-17 16:02:25 +0800

    03-17-24 16:02

--- src/commands/music.ts
+++ src/commands/music.ts
@@ -1,21 +1,50 @@
 import ytdl from "ytdl-core";
-import { createReadStream, createWriteStream, existsSync, mkdirSync, unlink } from "fs";
+import {
+	createReadStream,
+	createWriteStream,
+	existsSync,
+	mkdirSync,
+	unlink,
+} from "fs";
 
 const YoutubeMusicApi = require("youtube-music-api");
 const yt = new YoutubeMusicApi();
 
 export async function main(api: any, event: any, regex: any) {
-	const data = regex[1]
+	const data = regex[1];
+	const ytFormat1 = /youtube\.com\/watch\?v=([a-zA-Z0-9\-_]{11}$)/gi;
+	const ytFormat2 = /youtu\.be\/([a-zA-Z0-9\-_]+)/gi;
 
 	let music: any = {
 		content: [],
-	}
+	};
+
+	// if (ytFormat1.test(data)) {
+	// 	music = {
+	// 		content: [
+	// 			{
+	// 				videoId: data.match(ytFormat1)[1],
+	// 			},
+	// 		],
+	// 	};
+	// } else if (ytFormat2.test(data)) {
+	// 	music = {
+	// 		content: [
+	// 			{
+	// 				videoId: data.match(ytFormat2)[1],
+	// 			},
+	// 		],
+	// 	};
+	// } else {
 	await yt.initalize();
 	music = await yt.search(data.replace(/[^\w\s]/gi, ""), "video");
+	// }
 
 	const url = `https://www.youtube.com/watch?v=${music.content[0].videoId}`;
 	const info = await ytdl.getInfo(url);
-	const file = `${__dirname}/../../temp/${event.chat.id}/${info.videoDetails.title.replace(/\//gi, "")}.mp3`;
+	const file = `${__dirname}/../../temp/${
+		event.chat.id
+	}/${info.videoDetails.title.replace(/\//gi, "")}.mp3`;
 
 	if (music.content.length <= 0) {
 		return api.sendMessage(event.chat.id, "There's no video found.");
@@ -26,25 +55,33 @@ export async function main(api: any, event: any, regex: any) {
 			return api.sendMessage(
 				event.chat.id,
 				"Your request is still in process.",
-			)
+			);
 		}
 
-		if(!existsSync(`${__dirname}/../../temp/${event.chat.id}`)){
-			mkdirSync(`${__dirname}/../../temp/${event.chat.id}`)
+		if (!existsSync(`${__dirname}/../../temp/${event.chat.id}`)) {
+			mkdirSync(`${__dirname}/../../temp/${event.chat.id}`);
 		}
 
-		const file2 = createWriteStream(`temp/${event.chat.id}/${info.videoDetails.title.replace(/\//gi, "")}.mp3`);
+		const file2 = createWriteStream(
+			`temp/${event.chat.id}/${info.videoDetails.title.replace(
+				/\//gi,
+				"",
+			)}.mp3`,
+		);
 		ytdl(url, {
-			quality: "lowest",
+			quality: "lowestaudio",
 		})
-		.pipe(file2)
-		.on("finish", async () => {
-			api.sendAudio(event.chat.id, createReadStream(file).on("end", () => {
-				if (existsSync(file)) {
-					unlink(file, (error) => {})
-					api.deleteMessage(event.chat.id, event.message_id)
-				}
-			}))
-		})
+			.pipe(file2)
+			.on("finish", async () => {
+				api.sendAudio(
+					event.chat.id,
+					createReadStream(file).on("end", () => {
+						if (existsSync(file)) {
+							unlink(file, (error) => {});
+							api.deleteMessage(event.chat.id, event.message_id);
+						}
+					}),
+				);
+			});
 	}
 }
--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,46 @@
-const tg = require("node-telegram-bot-api")
-import { rm, mkdirSync, readFileSync, writeFileSync, existsSync } from "fs"
+const tg = require("node-telegram-bot-api");
+import { rm, mkdirSync, readFileSync, writeFileSync, existsSync } from "fs";
 
-const token = "API KEY From BotFather"
+const token = "Access token here"
 const bot = new tg(token, {
-	polling: true
-})
+	polling: true,
+});
 
-const json = JSON.parse(readFileSync(`${__dirname}/../config.json`, "utf-8"))
+const json = JSON.parse(readFileSync(`${__dirname}/../config.json`, "utf-8"));
 
-import { main } from './commands/music'
+import { main } from "./commands/music";
 
 const start = async () => {
-	if(existsSync(`${__dirname}/../temp`)){
+	if (existsSync(`${__dirname}/../temp`)) {
 		rm(`${__dirname}/../temp`, { recursive: true }, (error: any) => {
-				if(error) return console.error(`Pre may error ${JSON.stringify(error)}`)
-				setTimeout(() => {
-					mkdirSync(`${__dirname}/../temp`)
-				}, 500)
-			}
-			)
-	}else{
-		mkdirSync(`${__dirname}/../temp`)
+			if (error)
+				return console.error(`Pre may error ${JSON.stringify(error)}`);
+			setTimeout(() => {
+				mkdirSync(`${__dirname}/../temp`);
+			}, 500);
+		});
+	} else {
+		mkdirSync(`${__dirname}/../temp`);
 	}
 
 	bot.onText(/!music\s([\w\W]+)/gi, (msg: any, match: RegExp) => {
-		main(bot, msg, match)
-		json[msg.chat.id] = "music"
-		writeFileSync(`${__dirname}/../config.json`, JSON.stringify(json), "utf-8")
-	})
-	
-	bot.onText(/([\w\W]+)/gi, (msg: any, match: RegExp) => {
-		if(json[msg.chat.id] && msg.chat.id == msg.from.id){
-			main(bot, msg, match)
-		}
-	})
-}
+		main(bot, msg, match);
+		json[msg.chat.id] = "music";
+		writeFileSync(
+			`${__dirname}/../config.json`,
+			JSON.stringify(json),
+			"utf-8",
+		);
+	});
 
-start()
+	bot.onText(/([\w\W]+)/gi, (msg: any, match: RegExp) => {
+		if (json[msg.chat.id] && msg.chat.id == msg.from.id) {
+			main(bot, msg, match);
+		}
+	});
+};
+
+start();
 setInterval(() => {
-	console.log("Yamete")
-}, 1000)
+	console.log("Yamete");
+}, 1000);
